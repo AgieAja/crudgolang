f62dfd6081c17e8700db9679701f4f7cf1973086
tiar <tiar@yamisok.com>
2019-06-11 07:28:23 +0700

    Update phoneBookInterface.go

--- interface/phonebookinterface/phoneBookInterface.go
+++ interface/phonebookinterface/phoneBookInterface.go
@@ -1,1 +1,14 @@
 package phonebookinterface
+
+import (
+	phoneModel "crudgolang/model/phonebook"
+)
+
+//PhoneBookI - phone book interface
+type PhoneBookI interface {
+	CreateData(string, string) error
+	UpdateData(int64, string, string) error
+	DeleteData(int64) error
+	FindAll() ([]phoneModel.PhoneBooks, error)
+	FindByID(int64) (phoneModel.PhoneBooks, error)
+}
