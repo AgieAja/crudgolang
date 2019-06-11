c0a65967bea3cf33d6486359a537bff9989a1cef
tiar <tiar@yamisok.com>
2019-06-11 07:28:26 +0700

    Update phonebook.go

--- model/phonebook/phonebook.go
+++ model/phonebook/phonebook.go
@@ -1,1 +1,12 @@
 package phonebook
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+//PhoneBooks - maping table phone_books
+type PhoneBooks struct {
+	gorm.Model
+	Name        string
+	PhoneNumber string
+}
