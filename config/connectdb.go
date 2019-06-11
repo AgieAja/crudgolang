3096dffe1614259bfc03ab67f83299b2a4143cdd
tiar <tiar@yamisok.com>
2019-06-11 07:17:27 +0700

    Create connectdb.go

---
+++ config/connectdb.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	mydb   *gorm.DB
+	sqlErr error
+)
+
+//InitConnMySQLDB - preparation connection database mysql
+func InitConnMySQLDB() {
+	dbHost := "127.0.0.1"
+	dbPort := "3306"
+	dbUser := "root"
+	dbPass := "tiara1!@#"
+	dbName := "test"
+
+	desc := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	mydb, sqlErr = createConnMySQL(desc)
+}
+
+//GetMySQLDB - get connection db mysql
+func GetMySQLDB() (*gorm.DB, error) {
+	return mydb, sqlErr
+}
+
+//createConnMySQL - create connection database mysql
+func createConnMySQL(desc string) (*gorm.DB, error) {
+	mydb, sqlErr = gorm.Open("mysql", desc)
+	if sqlErr != nil {
+		return nil, sqlErr
+	}
+
+	sqlErr = mydb.DB().Ping()
+	if sqlErr != nil {
+		return nil, sqlErr
+	}
+
+	mydb.DB().SetMaxIdleConns(10)
+	mydb.DB().SetMaxOpenConns(10)
+	return mydb, nil
+}
