7cdf2fb4b186f9b52925fc16956c71878fe319cb
tiar <tiar@yamisok.com>
2019-06-11 09:08:15 +0700

    Update phoneBookUCInterface.go

--- interface/phonebookucinterface/phoneBookUCInterface.go
+++ interface/phonebookucinterface/phoneBookUCInterface.go
@@ -1,1 +1,14 @@
 package phonebookucinterface
+
+import (
+	phoneModel "crudgolang/model/phonebook"
+)
+
+//PhoneBookUCI - phone book usecase interface
+type PhoneBookUCI interface {
+	InsertData(string, string) error
+	EditData(int64, string, string) error
+	DeleteData(int64) error
+	GetAll() ([]phoneModel.PhoneBooks, error)
+	GetByID(int64) (phoneModel.PhoneBooks, error)
+}
