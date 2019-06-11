5cfa01b969ab457c645dea492fd352f507656f26
tiar <tiar@yamisok.com>
2019-06-11 10:14:30 +0700

    Update phonebook.go

--- model/phonebook/phonebook.go
+++ model/phonebook/phonebook.go
@@ -7,6 +7,12 @@ import (
 //PhoneBooks - maping table phone_books
 type PhoneBooks struct {
 	gorm.Model
+	Name        string
+	PhoneNumber string
+}
+
+//Datas - struct for json
+type Datas struct {
 	Name        string `json:"name"`
-	PhoneNumber string `json:"phone_number"`
+	PhoneNumber string `json:"phone"`
 }
